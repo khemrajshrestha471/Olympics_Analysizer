bb302a176e1f1340efc5bcc1b6e155d250f54c29
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-04-24 17:42:26 +0545

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    # medal_tally['Gold'] = medal_tally['Gold'].astype('int')
+    # medal_tally['Silver'] = medal_tally['Silver'].astype('int')
+    # medal_tally['Bronze'] = medal_tally['Bronze'].astype('int')
+    # medal_tally['total'] = medal_tally['total'].astype('int')
